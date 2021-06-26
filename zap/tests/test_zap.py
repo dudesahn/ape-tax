7f236abfcc84fdbb595e4396cc337c4053e129df
dudesahn <dudesahn@gmail.com>
2021-06-26 11:55:58 -0400

    Update test_zap.py

--- zap/tests/test_zap.py
+++ zap/tests/test_zap.py
@@ -9,6 +9,7 @@ def test_3crv_zapper(zap_3crv, lp_3crv, y3crv, vault, accounts, yvecrv_holder):
     lp_3crv.approve(zap_3crv, 2 ** 256 - 1, {"from": yvecrv_holder})
     zap_3crv.zap({"from": yvecrv_holder})
     assert y3crv.balanceOf(yvecrv_holder) > before
-    assert lp_3crv.balanceOf(yvecrv_holder) == before_lp_3Crv
+    assert lp_3crv.balanceOf(yvecrv_holder) == 0
     assert lp_3crv.balanceOf(zap_3crv) == 0
     assert y3crv.balanceOf(zap_3crv) == 0
+    print("Starting 3Crv Balance:", before_lp_3Crv, "\nStarting yVault Balance:", before, "\nEnding yVault Balance:", y3crv.balanceOf(yvecrv_holder))
