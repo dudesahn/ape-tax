68c48c7c62e64994fc4b36565067d6f230bcfc39
dudesahn <dudesahn@gmail.com>
2021-06-26 11:19:15 -0400

    feat: updates

--- zap/tests/conftest.py
+++ zap/tests/conftest.py
@@ -8,35 +8,35 @@ def shared_setup(fn_isolation):
 
 @pytest.fixture
 def user(accounts):
-    return accounts.at("0xfd875403b4338a1fa9dcb03AE4d49ED43a9290Bb", force=True)
+    yield accounts.at("0xfd875403b4338a1fa9dcb03AE4d49ED43a9290Bb", force=True)
 
 
 @pytest.fixture
 def crv():
-    return Contract("0xD533a949740bb3306d119CC777fa900bA034cd52")
+    yield Contract("0xD533a949740bb3306d119CC777fa900bA034cd52")
 
 
 @pytest.fixture
 def lp_3crv():
-    return Contract("0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490")
+    yield Contract("0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490")
 
 @pytest.fixture
 def y3crv():
-    return Contract("0x84E13785B5a27879921D6F685f041421C7F482dA")
+    yield Contract("0x84E13785B5a27879921D6F685f041421C7F482dA")
 
 
 @pytest.fixture
 def vault():
-    return Contract("0xc5bDdf9843308380375a611c18B50Fb9341f502A")
+    yield Contract("0xc5bDdf9843308380375a611c18B50Fb9341f502A")
 
 
 @pytest.fixture
 def vesting():
-    return Contract("0x575CCD8e2D300e2377B43478339E364000318E2c")
+    yield Contract("0x575CCD8e2D300e2377B43478339E364000318E2c")
 
 @pytest.fixture
 def minter(interface):
-    return Contract("0xd061D61a4d941c39E5453435B6345Dc261C2fcE0")
+    yield Contract("0xd061D61a4d941c39E5453435B6345Dc261C2fcE0")
 
 
 @pytest.fixture
